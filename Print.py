7ecfd2fea9404d3369ca336feed6c470fc2cfd60
Ruthwik <chittetiruthwik@gmail.com>
2024-09-19 16:02:38 +0530

    Update Print.py

--- Print.py
+++ Print.py
@@ -1,3 +1,1 @@
-'''Lesson 1 : How to PRINT!
-The below line has This is how we should print that you can write whatever you wanna write but don't forget the double quotes.'''
-print("This how we should print")
+print("Hello World")
